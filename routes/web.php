dca9fdb18977e4487a5e2b6d45618681130fa0be
Bartosz Woldanski <studio@dziwnykot.pl>
2024-03-21 19:43:10 +0100

    Change routes definitions.
    Default Dashboard page is now off. Welcome is now
    a landing page at start, after login and logout
    process (it handles GET and POST).
    
    'auth' middleware routes definition from web.php
    is moved  into similar definition in auth.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -14,25 +15,23 @@ use Inertia\Inertia;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+ return redirect('welcome');
 });
+
+Route::get('/welcome', [WelcomeController::class, 'index'])->name('welcome');
+
+
+Route::post('/welcome', [AuthenticatedSessionController::class, 'destroy']
+)->name('welcome.logout');
+
+
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+ return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
