ac021a2ee2f18ed1bde103d39ed0b259792dbe9c
Bartosz Woldanski <studio@dziwnykot.pl>
2024-03-26 14:17:25 +0100

    Add new columns in User model
    The first column is "discount" value for any client, second is list of
    products that can be discounted by 'discount' value. "Discount" default
    value is 0, list can be set as null.
    In model this columns are added as fillable. For this is created seeder.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('discount')->default(0);
+            $table->json('discount_products_list')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/RegisteredUserSeeder.php
+++ database/seeders/RegisteredUserSeeder.php
@@ -14,9 +14,50 @@ class RegisteredUserSeeder extends Seeder
  public function run(): void
  {
   DB::table('users')->insert([
-   'name'     => "bfagencja",
-   'email'    => "bf@test.pl",
-   'password' => Hash::make('test'),
+   'name'                   => "bfagencja",
+   'email'                  => "bf@test.pl",
+   'password'               => Hash::make('test'),
+   'discount'               => 10,
+   'discount_products_list' => "{list: [
+'a4_black_print_prices',
+'a4_color_print_prices',
+'a4_color_gray_print_prices',
+'a4_color_plot_cut_price',
+'bc40_print_prices',
+'bc44_print_prices',
+'bc_lamin_prices',
+'bc_lamin_mix_prices',
+'bc_lamin_st_prices',
+'lf_print_min_price',
+'lf_paper_print_prices',
+'lf_foil_print_prices',
+'lf_foil_ed_print_prices',
+'lf_block_print_prices',
+'lf_canvas_print_prices',
+'lf_plot_cut__min_price',
+'lf_plot_cut_price',
+'lf_non_print_cut_price',
+'lf_transfer_foil_price',
+'roll_lamin_min_price',
+'lf_roll_lamin_price',
+'a4_roll_lamin_prices',
+'a4_roll_lamin_st_prices',
+'fold_prices',
+'staple_prices',
+'staple_binder_prices',
+'cut_price',
+'hole_prices',
+'rounded_corners_prices',
+'binding_metal_prices',
+'binding_plastic_prices',
+
+}",
+
+  ]);
+
+  DB::table('users')->insert(['name' => "bart",
+   'email'                            => "studio@dziwnykot.pl",
+   'password'                         => Hash::make('testtest'),
   ]);
 
  }
