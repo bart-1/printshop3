7884e31f3f95c423fc4c57adf1a4d746222b7740
Bartosz Woldanski <studio@dziwnykot.pl>
2024-05-12 19:32:23 +0200

    Add new tests for calcImposition function

--- tests/_tests_/calcImposition.test.ts
+++ tests/_tests_/calcImposition.test.ts
@@ -6,5 +6,25 @@ describe("imposition calc module", () => {
         expect(calcImposer(320, 450, 105, 148, 10, 4)).toBe(8);
     });
 });
+describe("imposition calc module", () => {
+    test("imposition of A5 on SRA3 sheet gives equal 4", () => {
+        expect(calcImposer(320, 450, 148, 210, 10, 4)).toBe(4);
+    });
+});
+describe("imposition calc module", () => {
+    test("imposition of A5R on SRA3 sheet gives equal 4", () => {
+        expect(calcImposer(320, 450, 210, 148, 10, 4)).toBe(4);
+    });
+});
+describe("imposition calc module", () => {
+    test("imposition of DL on SRA3 sheet gives equal 6", () => {
+        expect(calcImposer(320, 450, 99, 210, 10, 4)).toBe(6);
+    });
+});
+describe("imposition calc module", () => {
+    test("imposition of 100x70 on SRA3 sheet gives equal 18", () => {
+        expect(calcImposer(320, 450, 100, 70, 10, 4)).toBe(18);
+    });
+});
 
 
